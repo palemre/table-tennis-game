0553bbffdaeeeb549c367182113396eb49e71a99
Emre Palandöken <emre.palandoken@comkeys.group>
2020-04-06 17:20:40 +0200

    :sparkles: Gameplay functional

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function drawRect(x, y, w, h, color)
 
 // create the net
 const net = {
-    x : canvas.width - 1,
+    x : canvas.width/2,
     y : 0,
     width : 2,
     height : 10,
@@ -98,3 +98,57 @@ function render()
     drawCircle(ball.x, ball.y, ball.radius, ball.color)
 }
 
+// user control
+canvas.addEventListener('mousemove', movePaddle)
+
+function movePaddle(e)
+{
+    let rect = canvas.getBoundingClientRect()
+    user.y = e.clientY - rect.top - user.height/2
+}
+
+// collision detection
+function collision(b, p) {
+    b.top = b.y - b.radius
+    b.bottom = b.y + b.radius
+    b.left = b.x - b.radius
+    b.right = b.x + b.radius
+
+    p.top = p.y
+    p.bottom = p.y + p.height
+    p.left = p.x
+    p.right = p.x + p.width
+
+    return b.right > p.left && b.bottom > p.top && b.left < p.right && b.top < p.bottom
+}
+
+
+// update
+function update() {
+    ball.x += ball.velocityX
+    ball.y += ball.velocityY
+
+    // computer paddle
+    let computerLevel = 0.1
+    com.y += (ball.y - (com.y + com.height/2)) * computerLevel
+
+    if(ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0)
+    {
+        ball.velocityY = -ball.velocityY
+    }
+
+    let player = (ball.x < canvas.width/2) ? user : com
+    if(collision(ball,player)) {
+        ball.velocityX = -ball.velocityX
+    }
+}
+
+// game init
+function game() {
+    update()
+    render()
+}
+
+// loop
+const framePerSecond = 50
+setInterval(game, 1000/framePerSecond)
